b91d144e97bd79bf43615dc3c550bbe69be41060
Clayson Nardino <Forwest3@gmail.com>
2022-09-08 11:10:34 -0300

    mudança de nomes template

--- django/app1/urls.py
+++ django/app1/urls.py
@@ -3,14 +3,14 @@ from . import views
 
 urlpatterns = [
     path('', views.helloworld),
-    path('empresas/', views.empresas),
-    path('pessoas/', views.pessoas),
+    path('empresas/list/', views.empresas),
+    path('pessoas/list/', views.pessoas),
     path('ag', views.ag),
     path('sobre', views.sobre),
-    path('pessoas/add', views.pessoa_add),
-    path('pessoas/edit/<int:id>', views.pessoa_edit, name="edit-pessoa"),
-    path('pessoas/delete/<int:id>', views.pessoa_delete, name="delete-pessoa"),
-    path('empresas/add', views.empresa_add),
-    path('empresas/edit/<int:id>', views.empresa_edit, name="edit-empresa"),
-    path('empresas/delete/<int:id>', views.empresa_delete, name="delete-empresa"),
+    path('pessoa/add', views.pessoa_add),
+    path('pessoa/edit/<int:id>', views.pessoa_edit, name="edit-pessoa"),
+    path('pessoa/delete/<int:id>', views.pessoa_delete, name="delete-pessoa"),
+    path('empresa/add', views.empresa_add),
+    path('empresa/edit/<int:id>', views.empresa_edit, name="edit-empresa"),
+    path('empresa/delete/<int:id>', views.empresa_delete, name="delete-empresa"),
 ]
