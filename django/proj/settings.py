26ed1c3f4a9dfda324a2a34657fa54596eed04c9
Douglas Bitencourt Cardoso <104370128+bitencourtdoug@users.noreply.github.com>
2022-09-16 08:26:17 -0300

    login

--- django/proj/settings.py
+++ django/proj/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -151,7 +151,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ACCOUNT_LOGOUT_REDIRECT_URL = '/accounts/login/'  
-LOGIN_REDIRECT_URL = '/admin/' 
+LOGIN_REDIRECT_URL = '/accounts/logout/' 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
