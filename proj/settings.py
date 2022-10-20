f36e59eec2ac84f4b0d7f4272d99ad060f63c588
João Vítor Rios Fuck <jv-rf@hotmail.com>
2022-10-20 11:43:51 -0300

    identação e debug

--- proj/settings.py
+++ proj/settings.py
@@ -20,7 +20,7 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = True
+# DEBUG = True
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -110,7 +110,7 @@ except:
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+    }
 
 
 
